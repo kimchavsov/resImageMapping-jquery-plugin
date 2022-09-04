6665bf6822c07a80affa7503bafbfb44ceae0e92
Kimchav Sov <kimchavsov@gmail.com>
2022-09-05 00:27:29 +0800

    fix - remove deprecated

---
+++ resImageMap.js
@@ -0,0 +1,33 @@
+/*
+* resImageMap javascript
+*
+* A responsive image mapping plugin which help resizing the mapping area coordinates to match the actual image size on load and window.resize
+*
+*/
+
+function resImageMaps(image) {
+  const img = image;
+  let width = image.width
+  let height = image.height
+  const map = img.useMap
+
+  let wPercent = width/100
+  let hPercent =  height/100
+
+
+  // Select individual area not the map function
+  document.querySelectorAll(img[map] > area).forEach(function() {
+    console.log(this.coords)
+  })
+
+  // todo adjust the coordinate accordingly
+  // test in uat site
+}
+
+function scaleImage(image) {
+
+}
+
+resImageMaps.prototype.resImageMap = function() {
+
+}
